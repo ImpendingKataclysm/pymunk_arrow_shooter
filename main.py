ef73cb75c00d1e82e72c2b2252a986e5272f1b03
ImpendingKataclysm <ktjanzen42@gmail.com>
2024-06-06 14:17:29 -0700

    Place the player avatar on the screen and move it around with the arrow keys/WASD keys, and save a screenshot when the player presses 'P'.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
 import pymunk
+import pymunk.pygame_util
+
+from typing import Optional
 
 from gui import Interface
+from player import Player
 
 
 class App:
@@ -12,13 +16,15 @@ class App:
         pygame.init()
 
         self.running: bool = False
+        self.fps: float = 0
 
         self.space = pymunk.Space()
         self.space.gravity = (0, 100)
 
         self.gui = Interface(self.space)
+        self.draw_options = pymunk.pygame_util.DrawOptions(self.gui.screen)
 
-        print(self.gui)
+        self.player: Optional[Player] = None
 
     def setup(self):
         """
@@ -26,6 +32,11 @@ class App:
         :return: None
         """
         self.running = True
+        self.fps = 0
+
+        self.player = Player()
+        self.player.place()
+        self.space.add(self.player, self.player.shape)
 
     def run(self):
         """
@@ -34,24 +45,46 @@ class App:
         """
         while self.running:
             for e in pygame.event.get():
-                if e.type == pygame.KEYDOWN:
-                    self.handle_key_input()
-                elif e.type == pygame.QUIT:
+                if e.type == pygame.QUIT:
                     self.running = False
+
+            self.handle_key_input()
 
             self.gui.clear()
 
+            self.space.debug_draw(self.draw_options)
             pygame.display.flip()
+
+            self.fps = 60
+            self.space.step(1.0 / self.fps)
 
     def handle_key_input(self):
         """
-        Handle events triggered by keypress inputs.
+        Handle events triggered by keypress inputs. Move the player with arrow
+        keys and WASD, and save a screenshot with P.
         :return: None
         """
         keys = pygame.key.get_pressed()
+        max_x = self.gui.screen_width
+        max_y = self.gui.screen_height
 
         if keys[pygame.K_ESCAPE] or keys[pygame.K_q]:
             self.running = False
+
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.player.move(0, -1, max_x, max_y)
+
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.player.move(0, 1, max_x, max_y)
+
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.player.move(-1, 0, max_x, max_y)
+
+        if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.player.move(1, 0, max_x, max_y)
+
+        if keys[pygame.K_p]:
+            pygame.image.save(self.gui.screen, 'shooter.png')
 
 
 def main():
---
+++ player.py
@@ -0,0 +1,49 @@
+import pymunk
+from pymunk.vec2d import Vec2d
+
+
+class Player(pymunk.Body):
+    """
+    Player avatar.
+    """
+    def __init__(self):
+        super(Player, self).__init__(body_type=pymunk.Body.KINEMATIC)
+
+        self.radius = 25
+        self.color = (255, 50, 50, 255)
+        self.start_x = 100
+        self.start_y = 500
+        self.speed = 0.5
+        self.friction = 0.5
+        self.elasticity = 0.9
+        self.shape = pymunk.Circle(self, self.radius)
+
+    def place(self):
+        """
+        Set the player avatar's color and initial position.
+        :return: None
+        """
+        self.shape.sensor = False
+        self.shape.color = self.color
+        self.position = self.start_x, self.start_y
+
+    def move(self, dx, dy, max_x, max_y):
+        """
+        Move the player avatar according to changes in x and y position.
+        :param dx: The change in x position
+        :param dy: The change in y position
+        :param max_x: Maximum x position
+        :param max_y: Maximum y position
+        :return: None
+        """
+        self.position += self.speed * Vec2d(dx, dy)
+
+        if self.position.x < 0:
+            self.position = Vec2d(max_x, self.position.y)
+        elif self.position.x > max_x:
+            self.position = Vec2d(0, self.position.y)
+
+        if self.position.y < 0:
+            self.position = Vec2d(self.position.x, max_y)
+        elif self.position.y > max_y:
+            self.position = Vec2d(self.position.x, 0)
